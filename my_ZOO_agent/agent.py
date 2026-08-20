bc59cf3302f0c23c633c9121484e5c40c19141f8
Domendra28 <domendrasahu13@gmail.com>
2026-08-20 13:31:06 +0530

    Update gitignore and secure env

--- my_ZOO_agent/agent.py
+++ my_ZOO_agent/agent.py
@@ -1,7 +1,7 @@
 from google.adk.agents import Agent 
 from google.adk.tools import AgentTool
 from google.adk.tools import google_search
-
+import zoneinfo
 from datetime import datetime
 # Make sure your tool decorator is imported, e.g., from google.adk import tool
 
@@ -10,6 +10,38 @@ from datetime import datetime
 def current_time() -> str:
     """Returns the current date and time for the agent."""
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def current_time(timezone_name: str) -> str:
+    """
+    Returns the current date and time for a specific timezone.
+    Args:
+        timezone_name: The standard IANA timezone name (e.g., 'Asia/Kolkata', 'Europe/London').
+    """
+    try:
+        # Uses Python's built-in timezone database I dont used any other library for timezone conversion.
+        tz = zoneinfo.ZoneInfo(timezone_name)
+        return datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S %Z")
+    except Exception as e:
+        return f"Error finding time for {timezone_name}: {str(e)}"
+
+
+
+  
+time_agent = Agent(
+    name="Current_time_agent",
+    model="gemini-2.5-flash-lite",
+    description="you are an AI assistant that help user in answering every type of the query.",
+    instruction="""
+    If the user asks for the time in a specific city, pass the city name directly to the current_time tool.
+    Give the final answer in only 2 lines.
+    """,
+    tools=[current_time]
+)
+time_agent_tool =AgentTool(time_agent)
+
+
+
 
 
 wikipedia_agent= Agent(
