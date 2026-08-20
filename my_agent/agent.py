5770a3da1dc06009fc59171cf921868bd057e9c3
Domendra28 <domendrasahu13@gmail.com>
2026-08-20 13:05:23 +0530

    structured the agents

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -12,16 +12,23 @@ def current_time() -> str:
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-
+wikipedia_agent= Agent(
+    name="Wikipedia_agent",
+    model="gemini-2.5-flash-lite",
+    description="You are an AI assistant that answers general knowledge questions by searching Wikipedia.",
+    instruction="Use Wikipedia to find answers and summarize them in 2 lines.",
+    tools=[google_search] # ONLY built-in tools here can be used, no custom tools are allowed in this agent
+)
+agent_tool = AgentTool(wikipedia_agent)
 
 root_agent = Agent(
-    name="Current_time_agent",
+    name="AI_ZOO_AGENT",
     model="gemini-3.1-flash-lite",
-    description="you are an AI assistant that help user in answering every type of the query.",
+    description="you are an AI ZOO guide assistant that help user in answering every type of the query.",
     instruction = """
-you are assistant to the user and you will answer  the questions and give answer in only 2 lines
+you are assistant to the user and you will first greet the user like welcome to our zoo lets explore the zoo or many creative greets, then answer  the questions and give answer in only 2 lines
 """,
-    tools=[current_time]
+    tools=[current_time, AgentTool(wikipedia_agent)]
 )
 
 
